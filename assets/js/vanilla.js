9c4755941d773abb99fbfdd998363de92a472b64
Inthone Vilayhong <Thonouu@gmail.com>
2022-04-22 15:03:44 +0200

    commit de mon formulaire

---
+++ assets/js/vanilla.js
@@ -0,0 +1,26 @@
+const $ = document; //! convention quand on est faineant
+
+$.addEventListener("DOMContentLoaded", () => {
+    //! on attend le chargement du DOM
+    $.querySelector("#contact-form").addEventListener("submit", async (e) => {
+        e.preventDefault(); //! on supprimer l'event par defaut
+
+        //!creation d'une const pour recuperer les infos input
+        const data = {
+            firstname: $.querySelector("#firstname").value,
+            lastname: $.querySelector("#lastname").value,
+            email: $.querySelector("#email").value,
+            sujet: $.querySelector("#subject").value,
+            message: $.querySelector("#message").value,
+        };
+        console.log(data); //! juste pour vérifier data que l'on recoit
+
+        //!Requete axios
+        const response = await axios.post("http://localhost:3000/form", data);
+
+        console.log(response.data);
+        alert("You're so good !");
+
+        location.reload(); //! Actualiser la page
+    });
+});
